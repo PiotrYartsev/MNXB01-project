46541472a2609c7b359f20c13a314ea11375e45e
Piotr Yartsev <piotr_1997@live.se>
2019-11-03 22:51:07 +0100

    Date_data not creating for Lulea, dont know why

--- CleanData/cleaning_data.sh
+++ CleanData/cleaning_data.sh
@@ -12,39 +12,51 @@ declare -a data=(smhi-opendata_Lulea.csv smhi-opendata_Lund.csv smhi-opendata_Vi
 cd datasets/
 
 for name in "${data[@]}"
-do
-echo "new"
-#if directory already exist for a data file remove it
-if [ -d ${name::-4} ]; then
-   rm -r ${name::-4}
-   echo "Direcetory ${name::-4} already exist, removing it and repalcing with new ${name::-4} directory"
-fi
-#create a directory for each data file to store the output
-mkdir ${name::-4}
-#create a textfile for storing cleaned data
-touch ${name::-4}/data_${name::-3}txt
-#create a textfile for storing information about the data
-touch ${name::-4}/info_${name::-3}txt
+	do
+	echo "new"
+	#if directory already exist for a data file remove it
+	if [ -d ${name::-4} ]; then
+	   rm -r ${name::-4}
+	   echo "Direcetory ${name::-4} already exist, removing it and repalcing with new ${name::-4} directory"
+	fi
+	#create a directory for each data file to store the output
+	mkdir ${name::-4}
+	#create a textfile for storing cleaned data
+	touch ${name::-4}/data_${name::-3}txt
+	#create a textfile for storing information about the data
+	touch ${name::-4}/info_${name::-3}txt
 
-#Put top 9 lines, containing all of the information about the data
-head -n 9 ${name} >> ${name::-4}/info_${name::-3}txt
+	#Put top 9 lines, containing all of the information about the data
+	head -n 9 ${name} >> ${name::-4}/info_${name::-3}txt
+
+	#for each line in cvs
+	while IFS=, read -r field1 
+	do
+	#clean lines that contain text after the data 
+	if [[ $field1 == *";;"* ]]; then
+		if [[ $field1 == *"G;;"* ]]; then
+		echo -n "$field1"|sed 's/;;.*//' >> ${name::-4}/data_${name::-3}txt
+		else
+		continue
+		fi
+	fi 
+
+	#add data from cvs to file named data_"name".txt
+	if [[ $field1 == *"G"* ]]; then
+	  echo -n "$field1 \n" >> ${name::-4}/data_${name::-3}txt
+	fi 
+	done < ${name}
+	touch ${name::-4}/data_date_${name::-3}txt
 
 
-while IFS=, read -r field1 
-do
-if [[ $field1 == *";;"* ]]; then
-	if [[ $field1 == *"G;;"* ]]; then
-    echo -n "$field1"|sed 's/;;.*//' >> ${name::-4}/data_${name::-3}txt
-fi 
-	continue
-fi 
+	touch ${name::-4}/data_date_${name::-3}txt
+	while IFS= read -r line
+	do
+		echo "hello"
+	  echo -n "$line" |sed 's/;.*//' >> ${name::-4}/data_date_${name::-3}txt
+	done < ${name::-4}/data_${name::-3}txt
 
 
-if [[ $field1 == *"G"* ]]; then
-  echo -n "$field1" >> ${name::-4}/data_${name::-3}txt
-fi 
- 
-done < ${name}
 done
 cd ..
 echo "done"
