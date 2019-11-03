bf8b9a1735c3bf059e62773a02d5c99b4cba03ac
Piotr Yartsev <piotr_1997@live.se>
2019-11-03 21:12:14 +0100

    fixed the cleaning code a bit, still not done

--- CleanData/cleaning_data.sh
+++ CleanData/cleaning_data.sh
@@ -1,68 +1,40 @@
 #!/bin/bash
 
-# this function prints an error with the information on how to run this
-# script.
-usage(){
-	echo "----"
-	echo -e "  Wrong syntax. To call this script please use"
-	echo -e "   $0 '<artistlastname>_<artistname>' ['<artistlastname>_<artistname>' ...]"
-	echo -e "  Example:"
-	echo -e "   $0 'Hubbard_Rob'"
-	echo "----"
-}
-
-
-
-####### CODE START #####################################################
-
-
-
 #List of all the data files that we will use in this project
-#declare -a data=(smhi-opendata_Lulea.csv smhi-opendata_Lund.csv smhi-opendata_Visby.csv smhi-opendata_Umea.csv smhi-opendata_Falsterbo.csv smhi-opendata_Soderarm.csv smhi-openda_Karlstad.csv smhi-opendata_Boras.csv smhi-opendata_Falun.csv)
+declare -a data=(smhi-opendata_Lulea.csv smhi-opendata_Lund.csv smhi-opendata_Visby.csv smhi-opendata_Umea.csv smhi-opendata_Falsterbo.csv smhi-opendata_Soderarm.csv smhi-openda_Karlstad.csv smhi-opendata_Boras.csv smhi-opendata_Falun.csv)
 
 
 #test list
-declare -a data=(smhi-opendata_Lulea.csv)
+#declare -a data=(smhi-opendata_Lulea.csv)
 
 
-
-echo "${data[0]}"
-#Check that all those files exist in the folder data.
-#cd datasets/
-#for n in ${data}
-#do
-#	if [ ! -e  ${data[n]} ];then
-#	echo -e "File ${n} is missing"
-#	exit 1
-#	fi
-#done
-#cd ..
-#STATSDIR="stats"
-
-#if [ -d "stats" ]; then
-#  rm -r stats
-#   echo "Direcetory stats already exist, removing it and repalcing with new stats directory"
-#fi
 
 cd datasets/
 
 for name in ${data}
 do
-
+#if directory already exist for a data file remove it
 if [ -d ${name::-4} ]; then
    rm -r ${name::-4}
    echo "Direcetory ${name::-4} already exist, removing it and repalcing with new ${name::-4} directory"
 fi
-
+#create a directory for each data file to store the output
 mkdir ${name::-4}
+#create a textfile for storing cleaned data
 touch ${name::-4}/data_${name::-3}txt
+#create a textfile for storing information about the data
 touch ${name::-4}/info_${name::-3}txt
 
+#Put top 9 lines, containing all of the information about the data
+head -n 9 ${name} >> ${name::-4}/info_${name::-3}txt
 
-echo $(sed -n "1p" "$name") >> ${name::-4}/info_${name::-3}txt
+
 while IFS=, read -r field1 
 do
 if [[ $field1 == *";;"* ]]; then
+	if [[ $field1 == *"G"* ]]; then
+    echo -n "$field1"|sed 's/;;.*//' >> ${name::-4}/data_${name::-3}txt
+fi 
 	continue
 fi 
 
