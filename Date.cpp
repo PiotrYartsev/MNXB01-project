80aa41cb29df0381a97f533a103833f2d0168b1e
b04505009 <b04505009@ntu.edu.tw>
2019-11-06 14:24:37 +0100

    Added day_of_year function to class Date

--- Date.cpp
+++ Date.cpp
@@ -41,9 +41,26 @@ public:
     {
         this->month = month;
     }
-    string toString()
+    string to_string()
     {
-        return to_string(year) + '-' + to_string(month) + '-' + to_string(day);
+        return std::to_string(year) + '-' + std::to_string(month) + '-' + std::to_string(day);
+    }
+    int day_of_year()
+    {
+        if (is_leap())
+        {
+            int leapYearDaysByMonth[] = {0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335};
+            return leapYearDaysByMonth[month - 1] + day;
+        }
+        else
+        {
+            int nonLeapYearDaysByMonth[] = {0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335};
+            return nonLeapYearDaysByMonth[month - 1] + day;
+        }
+    }
+    bool is_leap()
+    {
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
     }
 
 private:
